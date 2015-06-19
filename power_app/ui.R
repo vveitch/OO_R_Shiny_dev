add09b2178a16b779608e921d647c9924cfad2d0
vveitch <victorveitch@gmail.com>
2015-06-18 21:04:02 -0400

    changed h4 headers to h3

--- power_app/ui.R
+++ power_app/ui.R
@@ -17,21 +17,21 @@ shinyUI(
                           sidebarPanel(
                             tabsetPanel(
                               tabPanel("Main",
-                                       numericInput("sample_size", label = h3("Sample size"), value = 100, min=10, max=1000),
+                                       numericInput("sample_size", label = h4("Sample size"), value = 100, min=10, max=1000),
                                        
-                                       sliderInput("true_prob", label = h3("True probability p"), step=0.005,
+                                       sliderInput("true_prob", label = h4("True probability p"), step=0.005,
                                                    min = 0, max = 1, value = 0.6),
                                        
-                                       sliderInput("null_prob", label = HTML("<h3>Null hypothesis p<sub>0</sub></h3>"), step=0.005,
+                                       sliderInput("null_prob", label = HTML("<h4>Null hypothesis p<sub>0</sub></h4>"), step=0.005,
                                                    min = 0, max = 1, value = 0.5),
                                        
-                                       selectInput("hA", label = h3("Alternative hypothesis"), 
+                                       selectInput("hA", label = h4("Alternative hypothesis"), 
                                                    choices = 
                                                      list("p not equal p0" = "two.sided", "p<p0" = "less", "p>p0" = "greater"), 
                                                    selected = ">0.5"),
                                        
                                        sliderInput("sig_level", 
-                                                   label = HTML("<h3>Significance level &alpha;</h3>"),
+                                                   label = HTML("<h4>Significance level &alpha;</h4>"),
                                                    min = 0, 
                                                    max = 0.5, value = 0.05)
                               ),
@@ -66,21 +66,21 @@ shinyUI(
                             tabsetPanel(
                             tabPanel("Main",
                             
-                            numericInput("te.sample_size", label = h3("Sample size"), value = 100, min=10, max=1000),
+                            numericInput("te.sample_size", label = h4("Sample size"), value = 100, min=10, max=1000),
                             
-                            sliderInput("te.true_prob_A", label = HTML("<h3>True probability p<sub>A</sub></h3>"), step=0.005,
+                            sliderInput("te.true_prob_A", label = HTML("<h4>True probability p<sub>A</sub></h4>"), step=0.005,
                                         min = 0, max = 1, value = 0.6),
                             
-                            sliderInput("te.true_prob_B", label = HTML("<h3>True probability p<sub>B</sub></h3>"), step=0.005,
+                            sliderInput("te.true_prob_B", label = HTML("<h4>True probability p<sub>B</sub></h4>"), step=0.005,
                                         min = 0, max = 1, value = 0.4),
                             
-                            selectInput("te.hA", label = h3("Alternative hypothesis"), 
+                            selectInput("te.hA", label = h4("Alternative hypothesis"), 
                                         choices = 
                                           list("pA not equal pB" = "two.sided", "pA less than pB" = "less", "pA greater than pB" = "greater"), 
                                         selected = "two.sided"),
                             
                             sliderInput("te.sig_level", 
-                                        label = HTML("<h3>Significance level &alpha;</h3>"),
+                                        label = HTML("<h4>Significance level &alpha;</h4>"),
                                         min = 0, 
                                         max = 0.5, value = 0.05)
                             
@@ -109,23 +109,23 @@ shinyUI(
                         sidebarLayout(
                           sidebarPanel(
                             
-                            numericInput("tu.sample_size_A", label = h3("Sample size A"), value = 100, min=10, max=1000),
+                            numericInput("tu.sample_size_A", label = h4("Sample size A"), value = 100, min=10, max=1000),
                             
-                            numericInput("tu.sample_size_B", label = h3("Sample size B"), value = 100, min=10, max=1000),
+                            numericInput("tu.sample_size_B", label = h4("Sample size B"), value = 100, min=10, max=1000),
                             
-                            sliderInput("tu.true_prob_A", label = HTML("<h3>True probability p<sub>A</sub></h3>"), step=0.005,
+                            sliderInput("tu.true_prob_A", label = HTML("<h4>True probability p<sub>A</sub></h4>"), step=0.005,
                                         min = 0, max = 1, value = 0.6),
                             
-                            sliderInput("tu.true_prob_B", label = HTML("<h3>True probability p<sub>B</sub></h3>"), step=0.005,
+                            sliderInput("tu.true_prob_B", label = HTML("<h4>True probability p<sub>B</sub></h4>"), step=0.005,
                                         min = 0, max = 1, value = 0.4),
                             
-                            selectInput("tu.hA", label = h3("Alternative hypothesis"), 
+                            selectInput("tu.hA", label = h4("Alternative hypothesis"), 
                                         choices = 
                                           list("pA not equal pB" = "two.sided", "pA less than pB" = "less", "pA greater than pB" = "greater"), 
                                         selected = "two.sided"),
                             
                             sliderInput("tu.sig_level", 
-                                        label = HTML("<h3>Significance level &alpha;</h3>"),
+                                        label = HTML("<h4>Significance level &alpha;</h4>"),
                                         min = 0, 
                                         max = 0.5, value = 0.05)
                             
