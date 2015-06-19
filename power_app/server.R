da0b559372f95b1ac7622bbec51ae94c17e314c7
vveitch <victorveitch@gmail.com>
2015-06-18 20:49:08 -0400

    power app now allows comparison plots and only plots power values for np>10

--- power_app/server.R
+++ power_app/server.R
@@ -15,52 +15,108 @@ shinyServer(function(input, output, session) {
   ###logic for single (s) sample proportion test
   #this generates the function that relates sample size and power
   #ie. it's a (reactive) closure 
-  s.samp_pwr <- reactive({
-    function(n) {(pwr.p.test(h=ES.h(input$true_prob,input$null_prob),
-                             n=n,
-                             sig.level=input$sig_level,
-                             power=NULL,
-                             alternative=input$hA))$power}
-  })    
+  s.samp_pwr <-  reactive(
+    {function(n,null_prob,sig_level) {(pwr.p.test(h=ES.h(input$true_prob,null_prob),
+                                                                             n=n,
+                                                                             sig.level=sig_level,
+                                                                             power=NULL,
+                                                                             alternative=input$hA))$power}
+  });
+
   
-  output$summary <- renderPrint(format_output((s.samp_pwr())(input$sample_size)))
-                                      
+  output$summary <- renderPrint(format_output((s.samp_pwr())
+                                              (input$sample_size,input$null_prob,input$sig_level)))
+  
   
   output$s.powerPlot <- renderPlot({
-    ggplot(data.frame(x=c(1, 1000)), aes(x)) + stat_function(fun=s.samp_pwr()) + 
+    #normal inference only valid for np>10
+    smallestN = ceiling(max(10/input$true_prob,10/(1-input$true_prob)));    
+    #aesthetic options
+    powerPlot <- ggplot(data.frame(x=c(smallestN, smallestN+1000)), aes(x)) + 
       coord_cartesian(ylim = c(0, 1)) + 
-      labs(title='Power vs Sample Size',x="Sample Size (n)",y=paste("Power (1-", expression(beta),")")) +
-      theme(plot.title = element_text(size=20, face="bold", vjust=2))
+      labs(title='Power vs Sample Size',x="Sample Size (n)",
+           y=paste("Power (",expression(1-Beta),")")) +
+        #for reasons unknown actually having beta render slows things down a whole bunch
+           #y=expression(paste("Power (",1-Beta,")"))) +
+      theme(plot.title = element_text(size=20, face="bold", vjust=2));
+    
+    powerPlot <- powerPlot + stat_function(fun=s.samp_pwr(),
+                                           arg = list(null_prob=input$null_prob,
+                                                      sig_level=input$sig_level),
+                                           aes(colour = "Main"));
+    
+    if (1 %in% input$s.comparisonCheckGroup){
+      powerPlot<-powerPlot+stat_function(fun=s.samp_pwr(),
+                                          arg = list(null_prob=input$s.c1.null_prob,
+                                                     sig_level=input$s.c1.sig_level),
+                                         aes(colour = "Comparison 1"));
+    }
+    if (2 %in% input$s.comparisonCheckGroup){
+
+      powerPlot<-powerPlot+stat_function(fun=s.samp_pwr(),
+                                         arg = list(null_prob=input$s.c2.null_prob,
+                                                    sig_level=input$s.c2.sig_level),
+                                         aes(colour = "Comparison 2"));
+    }
+    powerPlot+scale_colour_manual("Legend",values=c("Main"="black",
+                                                    "Comparison 1"="red",
+                                                    "Comparison 2"="blue"))
   })
   
   ###logic for two equal (te) sample proportion test
-
+  
   te.samp_pwr <- reactive({
-    function(n) {(pwr.2p.test(h=ES.h(input$te.true_prob_A,input$te.true_prob_B),
-                             n=n,
-                             sig.level=input$te.sig_level,
-                             power=NULL,
-                             alternative=input$te.hA))$power}
+    function(n,sig_level) {(pwr.2p.test(h=ES.h(input$te.true_prob_A,input$te.true_prob_B),
+                              n=n,
+                              sig.level=sig_level,
+                              power=NULL,
+                              alternative=input$te.hA))$power}
   })    
   
-  output$te.summary <- renderPrint(format_output((te.samp_pwr())(input$te.sample_size)))
+  output$te.summary <- renderPrint(format_output((te.samp_pwr())
+                                                 (input$te.sample_size,input$te.sig_level)))
   
   
   output$te.powerPlot <- renderPlot({
-    ggplot(data.frame(x=c(1, 1000)), aes(x)) + stat_function(fun=te.samp_pwr()) + 
+    #normal inference only valid for np>10
+    smallestN = ceiling(max(10/input$te.true_prob_A,10/(1-input$te.true_prob_A),
+                            10/input$te.true_prob_B,10/(1-input$te.true_prob_B)));   
+    powerPlot <- ggplot(data.frame(x=c(smallestN, smallestN+1000)), aes(x)) + 
       coord_cartesian(ylim = c(0, 1)) + 
-      labs(title='Power vs Sample Size',x="Sample Size (n)",y=paste("Power (1-", expression(beta),")")) +
-      theme(plot.title = element_text(size=20, face="bold", vjust=2))
+      labs(title='Power vs Sample Size',x="Sample Size (n)",
+           y=paste("Power (",expression(1-Beta),")")) +
+      #for reasons unknown actually having beta render slows things down a whole bunch
+      #y=expression(paste("Power (",1-Beta,")"))) +
+      theme(plot.title = element_text(size=20, face="bold", vjust=2));
+    
+    powerPlot <- powerPlot + stat_function(fun=te.samp_pwr(),
+                                           arg = list(sig_level=input$sig_level),
+                                           aes(colour = "Main"));
+    
+    if (1 %in% input$te.comparisonCheckGroup){
+      powerPlot<-powerPlot+stat_function(fun=te.samp_pwr(),
+                                         arg = list(sig_level=input$te.c1.sig_level),
+                                         aes(colour = "Comparison 1"));
+    }
+    if (2 %in% input$te.comparisonCheckGroup){
+      
+      powerPlot<-powerPlot+stat_function(fun=te.samp_pwr(),
+                                         arg = list(sig_level=input$te.c2.sig_level),
+                                         aes(colour = "Comparison 2"));
+    }
+    powerPlot+scale_colour_manual("Legend",values=c("Main"="black",
+                                                    "Comparison 1"="red",
+                                                    "Comparison 2"="blue"))
   })
   
   ###logic for two unequal (tu) sample proportion test
   
   tu.samp_pwr <- reactive({
     function(nA,nB) {(pwr.2p2n.test(h=ES.h(input$tu.true_prob_A,input$tu.true_prob_B),
-                              n1=nA, n2=nB,
-                              sig.level=input$tu.sig_level,
-                              power=NULL,
-                              alternative=input$tu.hA))$power}
+                                    n1=nA, n2=nB,
+                                    sig.level=input$tu.sig_level,
+                                    power=NULL,
+                                    alternative=input$tu.hA))$power}
   })    
   
   output$tu.summary <- renderPrint(format_output((tu.samp_pwr())(input$tu.sample_size_A,input$tu.sample_size_B)))
@@ -69,13 +125,13 @@ shinyServer(function(input, output, session) {
     power <- vector(mode="double",length=10000);
     power<-vapply(1:10000, function(x) (tu.samp_pwr())(nA[x],nB[x]), 1.0);
     data.frame(nA,nB,power)
-    })
+  })
   
   output$tu.powerPlot <- renderPlot({
     ggplot(powerdf(), aes(nA,nB, z = power))+
-     geom_tile(aes(fill = power)) + stat_contour()
+      geom_tile(aes(fill = power)) + stat_contour()
   })
   
-  }
+}
 )    
 
--- power_app/ui.R
+++ power_app/ui.R
@@ -9,99 +9,131 @@ shinyUI(
   fluidPage(
     theme="bootswatch_cerulean.css",
     includeCSS("www/isim_header.css"),
-    isimHeaderPanel("Statistical Testing II"),
-      
-  navbarPage("Proportions",
-                   tabPanel("Single Sample",
-                            sidebarLayout(
-                              sidebarPanel(
-                                
-                                numericInput("sample_size", label = h3("Sample size"), value = 100, min=10, max=1000),
-                                
-                                sliderInput("true_prob", label = h3("True probability p"), step=0.005,
-                                            min = 0, max = 1, value = 0.6),
-                                
-                                sliderInput("null_prob", label = HTML("<h3>Null hypothesis p<sub>0</sub></h3>"), step=0.005,
-                                            min = 0, max = 1, value = 0.5),
-                                
-                                selectInput("hA", label = h3("Alternative hypothesis"), 
-                                            choices = 
-                                              list("p not equal p0" = "two.sided", "p<p0" = "less", "p>p0" = "greater"), 
-                                            selected = ">0.5"),
-                                
-                                sliderInput("sig_level", 
-                                            label = HTML("<h3>Significance level &alpha;:</h3>"),
-                                            min = 0, 
-                                            max = 0.5, value = 0.05)
+    isimHeaderPanel("Investigating Power"),
+    
+    navbarPage("Proportions",
+               tabPanel("Single Sample",
+                        sidebarLayout(
+                          sidebarPanel(
+                            tabsetPanel(
+                              tabPanel("Main",
+                                       numericInput("sample_size", label = h3("Sample size"), value = 100, min=10, max=1000),
+                                       
+                                       sliderInput("true_prob", label = h3("True probability p"), step=0.005,
+                                                   min = 0, max = 1, value = 0.6),
+                                       
+                                       sliderInput("null_prob", label = HTML("<h3>Null hypothesis p<sub>0</sub></h3>"), step=0.005,
+                                                   min = 0, max = 1, value = 0.5),
+                                       
+                                       selectInput("hA", label = h3("Alternative hypothesis"), 
+                                                   choices = 
+                                                     list("p not equal p0" = "two.sided", "p<p0" = "less", "p>p0" = "greater"), 
+                                                   selected = ">0.5"),
+                                       
+                                       sliderInput("sig_level", 
+                                                   label = HTML("<h3>Significance level &alpha;</h3>"),
+                                                   min = 0, 
+                                                   max = 0.5, value = 0.05)
                               ),
-                              mainPanel(
-                                verbatimTextOutput("summary"),
-                                plotOutput("s.powerPlot")
-                                
-                              )                            
-                            )
-                   ),
-                   tabPanel("Equal Sample Size Comparison",
-                            sidebarLayout(
-                              sidebarPanel(
-                                
-                                numericInput("te.sample_size", label = h3("Sample size"), value = 100, min=10, max=1000),
-                                
-                                sliderInput("te.true_prob_A", label = HTML("<h3>True probability p<sub>A</sub></h3>"), step=0.005,
-                                            min = 0, max = 1, value = 0.6),
-                                
-                                sliderInput("te.true_prob_B", label = HTML("<h3>True probability p<sub>B</sub></h3>"), step=0.005,
-                                            min = 0, max = 1, value = 0.4),
-                                
-                                #                                 sliderInput("te.effect_size", label = HTML("<h3>Null hypothesis effect size e<sub>0</sub></h3>"), 
-                                #                                             min = 0, max = 1, value = 0.5),
-                                
-                                selectInput("te.hA", label = h3("Alternative hypothesis"), 
-                                            choices = 
-                                              list("pA not equal pB" = "two.sided", "pA less than pB" = "less", "pA greater than pB" = "greater"), 
-                                            selected = "two.sided"),
-                                
-                                sliderInput("te.sig_level", 
-                                            label = HTML("<h3>Significance level &alpha;:</h3>"),
-                                            min = 0, 
-                                            max = 0.5, value = 0.05)
-                                
-                              ),
-                              mainPanel(
-                                verbatimTextOutput("te.summary"),
-                                plotOutput("te.powerPlot")
+                              tabPanel("Comparison Plots",
+                                       checkboxGroupInput("s.comparisonCheckGroup", label = h4("Include comparisons"), 
+                                                          choices = list("Comparison 1" = 1, "Comparison 2" = 2)),
+                                       sliderInput("s.c1.null_prob", label = HTML("<h4>p<sub>0</sub> for comparison 1</h4>"), step=0.005,
+                                                   min = 0, max = 1, value = 0.5),
+                                       sliderInput("s.c1.sig_level", 
+                                                   label = HTML("<h4>&alpha; for comparison 1</h4>"),
+                                                   min = 0, 
+                                                   max = 0.5, value = 0.05),
+                                       
+                                       sliderInput("s.c2.null_prob", label = HTML("<h4>p<sub>0</sub> for comparison 2</h4>"), step=0.005,
+                                                   min = 0, max = 1, value = 0.5),
+                                       sliderInput("s.c2.sig_level", 
+                                                   label = HTML("<h4>&alpha; for comparison 2</h4>"),
+                                                   min = 0, 
+                                                   max = 0.5, value = 0.05)
                               )
+                            )),
+                          mainPanel(
+                            verbatimTextOutput("summary"),
+                            plotOutput("s.powerPlot")
+                            
+                          )                            
+                        )
+               ),
+               tabPanel("Equal Sample Size Comparison",
+                        sidebarLayout(
+                          sidebarPanel(  
+                            tabsetPanel(
+                            tabPanel("Main",
+                            
+                            numericInput("te.sample_size", label = h3("Sample size"), value = 100, min=10, max=1000),
+                            
+                            sliderInput("te.true_prob_A", label = HTML("<h3>True probability p<sub>A</sub></h3>"), step=0.005,
+                                        min = 0, max = 1, value = 0.6),
+                            
+                            sliderInput("te.true_prob_B", label = HTML("<h3>True probability p<sub>B</sub></h3>"), step=0.005,
+                                        min = 0, max = 1, value = 0.4),
+                            
+                            selectInput("te.hA", label = h3("Alternative hypothesis"), 
+                                        choices = 
+                                          list("pA not equal pB" = "two.sided", "pA less than pB" = "less", "pA greater than pB" = "greater"), 
+                                        selected = "two.sided"),
+                            
+                            sliderInput("te.sig_level", 
+                                        label = HTML("<h3>Significance level &alpha;</h3>"),
+                                        min = 0, 
+                                        max = 0.5, value = 0.05)
+                            
+                            ),
+                            tabPanel("Comparison Plots",
+                                     checkboxGroupInput("te.comparisonCheckGroup", label = h4("Include comparisons"), 
+                                                        choices = list("Comparison 1" = 1, "Comparison 2" = 2)),
+                                     sliderInput("te.c1.sig_level", 
+                                                 label = HTML("<h4>&alpha; for comparison 1</h4>"),
+                                                 min = 0, 
+                                                 max = 0.5, value = 0.05),
+                                     
+                                     sliderInput("te.c2.sig_level", 
+                                                 label = HTML("<h4>&alpha; for comparison 2</h4>"),
+                                                 min = 0, 
+                                                 max = 0.5, value = 0.05)
                             )
-                   ),
-                   tabPanel("Unequal Sample Size Comparison",
-                            sidebarLayout(
-                              sidebarPanel(
-                                
-                                numericInput("tu.sample_size_A", label = h3("Sample size A"), value = 100, min=10, max=1000),
-                                
-                                numericInput("tu.sample_size_B", label = h3("Sample size B"), value = 100, min=10, max=1000),
-                               
-                                sliderInput("tu.true_prob_A", label = HTML("<h3>True probability p<sub>A</sub></h3>"), step=0.005,
-                                            min = 0, max = 1, value = 0.6),
-                                
-                                sliderInput("tu.true_prob_B", label = HTML("<h3>True probability p<sub>B</sub></h3>"), step=0.005,
-                                            min = 0, max = 1, value = 0.4),
-                                
-                                selectInput("tu.hA", label = h3("Alternative hypothesis"), 
-                                            choices = 
-                                              list("pA not equal pB" = "two.sided", "pA less than pB" = "less", "pA greater than pB" = "greater"), 
-                                            selected = "two.sided"),
-                                
-                                sliderInput("tu.sig_level", 
-                                            label = HTML("<h3>Significance level &alpha;:</h3>"),
-                                            min = 0, 
-                                            max = 0.5, value = 0.05)
-                                
-                              ),
-                              mainPanel(
-                                verbatimTextOutput("tu.summary"),
-                                 plotOutput("tu.powerPlot")
-                              )
-                            )
-                   )
-)))
+                            )),
+                          mainPanel(
+                            verbatimTextOutput("te.summary"),
+                            plotOutput("te.powerPlot")
+                          )
+                        )
+               ),
+               tabPanel("Unequal Sample Size Comparison",
+                        sidebarLayout(
+                          sidebarPanel(
+                            
+                            numericInput("tu.sample_size_A", label = h3("Sample size A"), value = 100, min=10, max=1000),
+                            
+                            numericInput("tu.sample_size_B", label = h3("Sample size B"), value = 100, min=10, max=1000),
+                            
+                            sliderInput("tu.true_prob_A", label = HTML("<h3>True probability p<sub>A</sub></h3>"), step=0.005,
+                                        min = 0, max = 1, value = 0.6),
+                            
+                            sliderInput("tu.true_prob_B", label = HTML("<h3>True probability p<sub>B</sub></h3>"), step=0.005,
+                                        min = 0, max = 1, value = 0.4),
+                            
+                            selectInput("tu.hA", label = h3("Alternative hypothesis"), 
+                                        choices = 
+                                          list("pA not equal pB" = "two.sided", "pA less than pB" = "less", "pA greater than pB" = "greater"), 
+                                        selected = "two.sided"),
+                            
+                            sliderInput("tu.sig_level", 
+                                        label = HTML("<h3>Significance level &alpha;</h3>"),
+                                        min = 0, 
+                                        max = 0.5, value = 0.05)
+                            
+                          ),
+                          mainPanel(
+                            verbatimTextOutput("tu.summary"),
+                            plotOutput("tu.powerPlot")
+                          )
+                        )
+               )
+    )))
